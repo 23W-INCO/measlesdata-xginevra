6a8e25837f8e913f86e276f46761c679718befce
Franzi <115472701+xginevra@users.noreply.github.com>
2023-11-13 11:32:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load measles cases data
-measles_data = pd.read_csv('geimpftekindernachbundesländern2015.csv')
+measles_data = pd.read_csv('fallzahlennachbundesland.csv')
 
 # Load vaccination rates data
-vaccination_data = pd.read_csv('fallzahlennachbundesland.csv')
+vaccination_data = pd.read_csv('geimpftekindernachbundesländern2015.csv')
 
+# Merge data
 merged_data = pd.merge(measles_data, vaccination_data, on='Bundesland')
 
-print(merged_data.head())
+# Explore data
+print(merged_data)
 
 # Bar chart for measles cases
 plt.figure(figsize=(12, 6))
-plt.bar(merged_data['bundesland'], merged_data['Masernzahlen'], label='Measles Cases')
+plt.bar(merged_data['Bundesland'], merged_data['Masernzahlen'], label='Measles Cases')
 plt.xlabel('Bundesland')
 plt.ylabel('Measles Cases')
 plt.title('Measles Cases per Bundesland in 2015')
 plt.legend()
+
+# Show the first plot
 plt.show()
 
 # Bar chart for vaccination rates
 plt.figure(figsize=(12, 6))
-plt.bar(merged_data['bundesland'], merged_data['impfrate'], label='Vaccination Rate')
+plt.bar(merged_data['Bundesland'], merged_data['Impfrate'], label='Vaccination Rate')
 plt.xlabel('Bundesland')
 plt.ylabel('Vaccination Rate')
 plt.title('Vaccination Rates per Bundesland in 2019')
 plt.legend()
+
+# Show the second plot
 plt.show()
