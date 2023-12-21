6ba662e26929171acd677a9b681db8f0d4922779
Franzi <115472701+xginevra@users.noreply.github.com>
2023-12-21 13:55:10 +0100

    hope nothing is broken

--- heatmap.py
+++ heatmap.py
@@ -12,7 +12,7 @@ del gdf['BEGINN']
 del gdf['WSK']
 
 # Load your CSV data
-csv_path = 'fallzahlennachbundesland.csv'
+csv_path = 'fallzahlennachbundesland.csv'  # Replace with the actual file name
 data = pd.read_csv(csv_path)
 
 # Merge GeoJSON data with CSV data on the common column
@@ -20,7 +20,7 @@ merged_data = gdf.merge(data, left_on='GEN', right_on='Bundesland')
 
 # Convert numeric columns to appropriate types
 merged_data['Masernzahlen'] = pd.to_numeric(merged_data['Masernzahlen'], errors='coerce')
-merged_data['Impfrate'] = pd.to_numeric(merged_data['Impfrate'].str.rstrip('%'), errors='coerce') / 100
+merged_data['Impfrate'] = pd.to_numeric(merged_data['Impfrate'], errors='coerce')
 merged_data['Per100000'] = pd.to_numeric(merged_data['Per100000'], errors='coerce')
 
 # Create a folium map centered around the mean latitude and longitude of the GeoJSON data
@@ -34,7 +34,7 @@ marker_cluster = MarkerCluster().add_to(m)
 folium.GeoJson(
     merged_data,
     name='Measles Data',
-    tooltip=folium.GeoJsonTooltip(fields=['Bundesland', 'Masernzahlen', 'Impfrate', 'Cases per 100000'],
+    tooltip=folium.GeoJsonTooltip(fields=['Bundesland', 'Masernzahlen', 'Impfrate', 'Per100000'],
                                   aliases=['Region', 'Measles Cases', 'Vaccination Rate', 'Cases per 100000'],
                                   localize=True),
 ).add_to(marker_cluster)
@@ -42,9 +42,9 @@ folium.GeoJson(
 # Create Choropleth layer for measles cases per 100000
 folium.Choropleth(
     geo_data=merged_data,
-    name='Per100000',
+    name='Cases per 100000',
     data=merged_data,
-    columns=['Bundesland', 'Cases per 100000'],
+    columns=['Bundesland', 'Per100000'],
     key_on='feature.properties.Bundesland',
     fill_color='YlOrRd',
     fill_opacity=0.7,
@@ -53,19 +53,30 @@ folium.Choropleth(
     highlight=True,
 ).add_to(m)
 
+# Create Choropleth layer for vaccination rate
+folium.Choropleth(
+    geo_data=merged_data,
+    name='Vaccination Rate',
+    data=merged_data,
+    columns=['Bundesland', 'Impfrate'],
+    key_on='feature.properties.Bundesland',
+    fill_color='Purples',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Vaccination Rate',
+    highlight=True,
+).add_to(m)
+
 # Add additional data as markers
 for idx, row in merged_data.iterrows():
     folium.Marker(
         location=[row.geometry.centroid.y, row.geometry.centroid.x],
-        popup=f"<strong>{row['GEN']}</strong><br>Population: {row['Gesamtbevoelkerung']}<br>Vaccination Rate: {row['Impfrate']:.2%}<br>Measles Cases: {row['Masernzahlen']}<br>Cases per 100000: {row['Cases per 100000']:.2f}",
-        icon=folium.Icon(color='blue'),
+        popup=f"<strong>{row['Bundesland']}</strong><br>Population: {row['Gesamtbevoelkerung']}<br>Vaccination Rate: {row['Impfrate']}<br>Measles Cases: {row['Masernzahlen']}<br>Cases per 100000: {row['Per100000']:.2f}",
+        icon=folium.Icon(color='pink'),
     ).add_to(marker_cluster)
 
 # Add Layer Control to toggle layers
 folium.LayerControl().add_to(m)
-
-# Add GeoJSON data to the map
-folium.GeoJson(merged_data).add_to(m)
 
 # Save the map as an HTML file
 m.save('bundeslaender_measles.html')
