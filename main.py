a4736b889de640e758bcd273f50485bec25a3efa
Franziska Wojtkowski <f.wojtk@web.de>
2024-01-20 21:26:55 +0100

    schaumermal

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import FileResponse
 import sqlite3
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
+from jsonindb import JsonInDatabaseTranformer
 
 app = FastAPI()
 
@@ -18,8 +19,9 @@ app.add_middleware(
 # Endpoint to serve the HTML file
 @app.get("/")
 def get_html():
+    json_in_db = JsonInDatabaseTranformer()
+    json_in_db.push_json_data_in_db()
     return FileResponse("./index.html")
-
 
 # Serve static files (HTML, JS, CSS)
 
